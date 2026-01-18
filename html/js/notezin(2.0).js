3680e903d44680d009133c451279249056ff9519
Nicolas <nicolasantos011@gmail.com>
2026-01-18 19:10:55 -0300

    up

--- html/js/notezin(2.0).js
+++ html/js/notezin(2.0).js
@@ -1,5 +1,26 @@
 
 
+const exercises = [
+    {
+        id: 1,
+        value: 'benchpress',
+        label: 'Bench Press',
+        muscle: 'chest',
+        enabled: true,
+        order: 1
+    },
+    {
+        id: 2,
+        value: 'Squat',
+        label: 'Squat',
+        muscle: 'legs',
+        enabled: true,
+        order: 2
+    }
+];
+
+
+
 function newRotine(){
     
     if(document.querySelectorAll('.rotine-content').length == 5){
@@ -9,36 +30,66 @@ function newRotine(){
     let original = document.getElementById('mold')
     let copy = original.cloneNode(true)
         copy.removeAttribute('id')  
+        copy.style.display = 'block'
    
     let place = document.getElementById('rotine-place')
         place.appendChild(copy)
 
     let nome = prompt("Digite o nome da rotina:");
-    if (nome == null) {
+    if (nome == null || nome == '') {
         copy.remove(true)
     }
     
 
     copy.querySelector('.training-name').textContent = nome
 
-    console.log( copy.querySelector('training-name'));
     
-    
+
+
 }
 
-function entryRotine(element) {
-    document.getElementById('rotine-place').style.display = 'none'
-    document.getElementById('one-rotine').style.display = 'block'
 
-    let lugar = document.getElementById('rotine-name')
-    
-    
-    lugar.textContent = element.textContent 
-}
 
-// Botao de voltar para as rotinas todos os treinos upper 1
-let backRotines = document.getElementById('back-rotines').addEventListener('click',() => {
-    document.getElementById('rotine-place').style.display = 'block'
-    document.getElementById('one-rotine').style.display = 'none'
 
-})
+
+
+
+
+
+
+
+
+
+
+
+/*
+const select = document.getElementById('exercise');
+exercises.forEach(ex => {
+            const option = document.createElement('option');
+            option.value = ex.value;  
+            option.textContent = ex.label;
+            select.appendChild(option);
+        });
+
+
+let NewExercise = document.getElementById('new-exercise')
+    NewExercise.addEventListener('click', ()=>{
+        const exercise = document.createElement('select')
+        const option = document.createElement('option');
+            option.value = '';  
+            option.textContent = 'Selecionar';
+            exercise.appendChild(option)
+
+            exercises.forEach(ex => {
+
+                const option = document.createElement('option');
+                option.value = ex.value;  
+                option.textContent = ex.label;
+                exercise.appendChild(option)
+            })
+
+        let place = document.getElementById('exercise')
+        place.appendChild(exercise)
+
+    })
+*/
